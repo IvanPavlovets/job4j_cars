c073f417ec9ebdd695602d38534ed3e15c39d70e
IvanPavlovets <ivan150287@gmail.com>
2022-09-28 12:31:59 +0700

    2. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/update_001.sql
@@ -0,0 +1,12 @@
+CREATE TABLE if not exists auto_user (
+  id SERIAL PRIMARY KEY,
+  login VARCHAR NOT NULL UNIQUE,
+  password VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE if not exists auto_post (
+  id SERIAL PRIMARY KEY,
+  text TEXT,
+  created TEXT,
+  auto_user_id INT NOT NULL REFERENCES auto_user(id)
+);
